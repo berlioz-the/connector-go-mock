aac8ec7f5a0eb1bb8039f51f92b727668abf0fd8
Ruben Hakopian <ruben.hakopian@gmail.com>
2018-07-03 18:11:46 -0700

    Separated sample projects

---
+++ sample-1/run.sh
@@ -0,0 +1,12 @@
+MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+MY_DIR="$(dirname $MY_PATH)"
+echo "My Dir: $MY_DIR"
+cd $MY_DIR
+
+berlioz local push-run --quick --cluster berliozgo --service example --pathoverride .,../support
+echo '==============================================================================='
+echo '==============================================================================='
+echo '==============================================================================='
+read -p "Pausing to fetch logs..." -t 2
+echo ""
+docker ps -a | grep berliozgo-example | head -n 1 | awk '{print $1}' | xargs docker logs
