4f703c068be6d849a3cd8947911db4c46d0adba6
Ruben Hakopian <ruben.hakopian@gmail.com>
2018-07-03 22:09:22 -0700

    change in local run script to support windows bash.

--- local-1/run.sh
+++ local-1/run.sh
@@ -3,12 +3,21 @@ MY_DIR="$(dirname $MY_PATH)"
 echo "My Dir: $MY_DIR"
 cd $MY_DIR
 
+dockerPrefix=""
+osNameStr=$(uname -r)
+echo "$osNameStr"
+if [[ "$osNameStr" = *'Microsoft'* ]]; then
+   echo "INSIDE BASH ON WINDOWS"
+   dockerPrefix=" -H tcp://0.0.0.0:2375"
+fi
+
 rsync -r ../../connector-go.git vendor/
 
-berlioz local push-run --quick --cluster berliozgo --service example --pathoverride .,../support,../../connector-go.git
+berlioz local push-run --quick --cluster berliozgo --service example --pathoverride .,../support
 echo '==============================================================================='
 echo '==============================================================================='
 echo '==============================================================================='
 read -p "Pausing to fetch logs..." -t 2
 echo ""
-docker ps -a | grep berliozgo-example | head -n 1 | awk '{print $1}' | xargs docker logs
+
+docker $dockerPrefix ps -a | grep berliozgo-example | head -n 1 | awk '{print $1}' | xargs docker $dockerPrefix logs
